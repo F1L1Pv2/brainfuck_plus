fa9188d643a1b04b722997049ff8de4b4cace2f9
F1L1P <mlodzik.filip@wp.pl>
2023-09-14 16:16:56 +0200

    moving into lexer parser ast style compiler

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
-use std::fs;
 use std::env;
-use std::io::Write;
+use std::fs;
 use std::fs::File;
+use std::io::Write;
 use std::path::Path;
 use std::process::exit;
+
+
+#[derive(Debug)]
+enum TokenType {
+    Word,
+    PointerLeft,
+    PointerRight,
+    PointerBack,
+    Add,
+    Sub,
+    ReadByte,
+    WriteByte,
+    MemAddr,
+    BaseMemAddr,
+    Syscall,
+    Clear,
+    ZeroJump,
+    NonZeroJump,
+}
+
+#[derive(Debug)]
+struct Token {
+    token_type: TokenType,
+    value: String,
+}
 
 fn main() {
     // read the file contents into a string from args
@@ -17,14 +42,12 @@ fn main() {
     let filename = &args[1];
 
     //check if extension is .bf
-    if !filename.ends_with(".bf"){
+    if !filename.ends_with(".bf") {
         println!("Brain fuck plus files must have .bf extension");
         exit(1);
     }
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
     let mut file_content: String = String::new();
     file_content.push_str("BITS 64\n");
@@ -35,115 +58,259 @@ fn main() {
     let len = contents.len();
 
     #[derive(PartialEq, Debug)]
-    struct Condition{
+    struct Condition {
         addr: usize,
     }
 
     #[derive(PartialEq, Debug)]
-    struct Forward{
-        back_addr: usize
+    struct Forward {
+        back_addr: usize,
     }
 
     #[derive(PartialEq, Debug)]
-    enum Jumps{
+    enum Jumps {
         Condition(Condition),
-        Forward(Forward)
+        Forward(Forward),
     }
 
     let mut jumps: Vec<Jumps> = Vec::new();
 
+    
     //cross reference
-    for i in 0..len{
+    for i in 0..len {
         let ch = contents.chars().nth(i).unwrap();
-        match ch{
-            '['=>{
+        match ch {
+            '[' => {
                 jumps.push(Jumps::Condition(Condition { addr: 0 }));
             }
-            ']'=>{
+            ']' => {
                 let len = jumps.len();
-                
+
                 //find the last condition
                 let mut last_condition = 0;
-                for j in (0..len).rev(){
-                    if jumps[j] == Jumps::Condition(Condition { addr: 0 }){
+                for j in (0..len).rev() {
+                    if jumps[j] == Jumps::Condition(Condition { addr: 0 }) {
                         last_condition = j;
                         break;
                     }
                 }
 
                 //set the address of the condition
-                match jumps[last_condition]{
+                match jumps[last_condition] {
                     Jumps::Condition(ref mut condition) => {
                         condition.addr = len;
                     }
-                    _=>{}
+                    _ => {}
                 }
 
                 //set the address of the forward to last condition
-                jumps.push(Jumps::Forward(Forward { back_addr: last_condition }));
+                jumps.push(Jumps::Forward(Forward {
+                    back_addr: last_condition,
+                }));
             }
-            _=>{}
+            _ => {}
         }
     }
 
     let mut last_condition = 0;
 
-    let mem_size = 1024*1024;
+    let mem_size = 1024 * 1024;
     let mut mem_dbg_ln = 0;
 
-    for i in 0..len{
+    let mut i: usize = 0;
+    
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut comment = false;
+
+    while i < len {
         let ch = contents.chars().nth(i).unwrap();
 
-        match ch{
-            '>'=>{
+        if !comment{
+
+        match ch {
+            '>' => {
+                tokens.push(Token {
+                    token_type: TokenType::PointerRight,
+                    value: ch.to_string(),
+                });
+            }
+            '<' => {
+                tokens.push(Token {
+                    token_type: TokenType::PointerLeft,
+                    value: ch.to_string(),
+                });
+            }
+            '+' => {
+                tokens.push(Token {
+                    token_type: TokenType::Add,
+                    value: ch.to_string(),
+                });
+            }
+            '-' => {
+                tokens.push(Token {
+                    token_type: TokenType::Sub,
+                    value: ch.to_string(),
+                });
+            }
+            '.' => {
+                tokens.push(Token {
+                    token_type: TokenType::WriteByte,
+                    value: ch.to_string(),
+                });
+            }
+            ',' => {
+                tokens.push(Token {
+                    token_type: TokenType::ReadByte,
+                    value: ch.to_string(),
+                });
+            }
+            '[' => {
+                tokens.push(Token {
+                    token_type: TokenType::ZeroJump,
+                    value: ch.to_string(),
+                });
+            }
+            ']' => {
+                tokens.push(Token {
+                    token_type: TokenType::NonZeroJump,
+                    value: ch.to_string(),
+                });
+            }
+            '$' => {
+                tokens.push(Token {
+                    token_type: TokenType::MemAddr,
+                    value: ch.to_string(),
+                });
+            }
+            '%' => {
+                tokens.push(Token {
+                    token_type: TokenType::BaseMemAddr,
+                    value: ch.to_string(),
+                });
+            }
+            '\'' => {
+                tokens.push(Token {
+                    token_type: TokenType::Clear,
+                    value: ch.to_string(),
+                });
+            }
+            '?' => {
+                tokens.push(Token {
+                    token_type: TokenType::Syscall,
+                    value: ch.to_string(),
+                });
+            }
+            '&' => {
+                tokens.push(Token {
+                    token_type: TokenType::PointerBack,
+                    value: ch.to_string(),
+                });
+            }
+
+            _ => {
+
+                if ch.is_whitespace(){
+                    i+=1;
+                    continue;
+                }
+
+                let next_char =  contents.chars().nth(i+1).unwrap();
+                let checker = ch.to_string() + next_char.to_string().as_str();
+
+                if checker == "/*"{
+                    comment = true;
+                    i+=2;
+                }else{
+                    i+=1;
+                }
+                continue;
+            }
+        }
+
+        i += 1;
+
+        }else{
+
+            let next_char =  contents.chars().nth(i+1).unwrap();
+
+            let checker = ch.to_string() + next_char.to_string().as_str();
+
+            if checker == "*/"{
+                comment = false;
+                i+=2;
+            }else{
+                i+=1;
+            }
+        }
+    }
+
+    // dbg!(&tokens);
+
+    // exit(1);
+
+    // for i in 0..len {
+    for token in tokens.iter(){
+        // let ch = contents.chars().nth(i).unwrap();
+
+        // match ch {
+        match token.token_type{
+            // '>' => {
+            TokenType::PointerRight => {
                 // file_content.push_str("    add QWORD[pointer], 1\n");
 
                 //check if pointer is at the end
-                file_content.push_str(format!("    cmp QWORD[pointer], {}\n", mem_size-1).as_str());
-                file_content.push_str(format!("    je bound_{}\n",i).as_str());
+                file_content
+                    .push_str(format!("    cmp QWORD[pointer], {}\n", mem_size - 1).as_str());
+                file_content.push_str(format!("    je bound_{}\n", i).as_str());
                 file_content.push_str("    add QWORD[pointer], 1\n");
-                file_content.push_str(format!("    jmp skip_{}\n",i).as_str());
-                file_content.push_str(format!("bound_{}:\n",i).as_str());
+                file_content.push_str(format!("    jmp skip_{}\n", i).as_str());
+                file_content.push_str(format!("bound_{}:\n", i).as_str());
                 file_content.push_str("    mov QWORD[pointer], 0\n");
-                file_content.push_str(format!("skip_{}:\n",i).as_str());
+                file_content.push_str(format!("skip_{}:\n", i).as_str());
             }
-            '<'=>{
-
+            // '<' => {
+            TokenType::PointerLeft => {
                 //check if pointer is zero
                 file_content.push_str("    cmp QWORD[pointer], 0\n");
-                file_content.push_str(format!("    je bound_{}\n",i).as_str());
+                file_content.push_str(format!("    je bound_{}\n", i).as_str());
                 file_content.push_str("    sub QWORD[pointer], 1\n");
-                file_content.push_str(format!("    jmp skip_{}\n",i).as_str());
-                file_content.push_str(format!("bound_{}:\n",i).as_str());
-                file_content.push_str(format!("    mov QWORD[pointer], {}\n", mem_size-1).as_str());
-                file_content.push_str(format!("skip_{}:\n",i).as_str());
-
-
+                file_content.push_str(format!("    jmp skip_{}\n", i).as_str());
+                file_content.push_str(format!("bound_{}:\n", i).as_str());
+                file_content
+                    .push_str(format!("    mov QWORD[pointer], {}\n", mem_size - 1).as_str());
+                file_content.push_str(format!("skip_{}:\n", i).as_str());
 
                 // file_content.push_str("    sub QWORD[pointer], 1\n");
             }
-            '$'=>{ // put current mem addr into cell
-                file_content.push_str(format!("debug_mem_{}:\n",mem_dbg_ln).as_str());
+            // '$' => {
+            TokenType::MemAddr =>{
+                // put current mem addr into cell
+                file_content.push_str(format!("debug_mem_{}:\n", mem_dbg_ln).as_str());
                 file_content.push_str("    mov rax, mem\n");
                 file_content.push_str("    add rax, QWORD[pointer]\n");
                 file_content.push_str("    mov rbx, rax\n");
                 file_content.push_str("    mov QWORD[rax], rbx\n");
-                mem_dbg_ln +=1 ;
+                mem_dbg_ln += 1;
             }
 
-            '%' => { // put base mem addr into cell
+            // '%' => {
+            TokenType::BaseMemAddr =>{
+                // put base mem addr into cell
                 file_content.push_str("    mov rax, mem\n");
                 file_content.push_str("    add rax, QWORD[pointer]\n");
                 file_content.push_str("    mov rbx, mem\n");
                 file_content.push_str("    mov QWORD[rax], rbx\n");
             }
 
-            '&' => { // set pointer to 0
+            // '&' => {
+            TokenType::PointerBack => {
+                // set pointer to 0
                 file_content.push_str("    mov QWORD[pointer], 0\n");
             }
 
-
-            '?'=>{ // perform syscall
+            // '?' => {
+            TokenType::Syscall => {
+                // perform syscall
                 file_content.push_str("    mov rbp, mem\n");
                 file_content.push_str("    add rbp, QWORD[pointer]\n");
                 file_content.push_str("    mov rax, QWORD[rbp]\n");
@@ -154,26 +321,30 @@ fn main() {
                 file_content.push_str("    add rbp, 8\n");
                 file_content.push_str("    mov rdx, QWORD[rbp]\n");
                 file_content.push_str("    syscall\n");
-                
             }
 
-            '\''=>{ // clear current cell
+            // '\'' => {
+            TokenType::Clear => {
+                // clear current cell
                 file_content.push_str("   mov rax, mem\n");
                 file_content.push_str("   add rax, QWORD[pointer]\n");
                 file_content.push_str("   mov BYTE[rax], 0\n");
             }
 
-            '+'=>{
+            // '+' => {
+            TokenType::Add => {
                 file_content.push_str("    mov rax, mem\n");
                 file_content.push_str("    add rax, QWORD[pointer]\n");
                 file_content.push_str("    add BYTE [rax], 1\n");
             }
-            '-'=>{
+            // '-' => {
+            TokenType::Sub => {
                 file_content.push_str("    mov rax, mem\n");
                 file_content.push_str("    add rax, QWORD[pointer]\n");
                 file_content.push_str("    sub BYTE [rax], 1\n");
             }
-            '.'=>{
+            // '.' => {
+            TokenType::WriteByte => {
                 file_content.push_str("    mov rax, 1\n");
                 file_content.push_str("    mov rdi, 1\n");
                 file_content.push_str("    mov rsi, mem\n");
@@ -181,7 +352,8 @@ fn main() {
                 file_content.push_str("    mov rdx, 1\n");
                 file_content.push_str("    syscall\n");
             }
-            ','=>{
+            // ',' => {
+            TokenType::ReadByte => {
                 file_content.push_str("    mov rax, 0\n");
                 file_content.push_str("    mov rdi, 0\n");
                 file_content.push_str("    mov rsi, mem\n");
@@ -189,68 +361,66 @@ fn main() {
                 file_content.push_str("    mov rdx, 1\n");
                 file_content.push_str("    syscall\n");
             }
-            '['=>{
+            // '[' => {
+            TokenType::ZeroJump => {
                 file_content.push_str("    mov rax, mem\n");
                 file_content.push_str("    add rax, QWORD[pointer]\n");
                 file_content.push_str("    mov al, byte[rax]\n");
                 file_content.push_str("    cmp al, 0\n");
 
                 let mut condition_id = 0;
-                for j in last_condition..len{
-                    if matches!(jumps[j], Jumps::Condition(_)){
+                for j in last_condition..len {
+                    if matches!(jumps[j], Jumps::Condition(_)) {
                         condition_id = j;
                         break;
                     }
                 }
 
-                last_condition = condition_id+1;
+                last_condition = condition_id + 1;
 
-                let condition = match jumps[condition_id]{
-                    Jumps::Condition(ref condition) => {
-                        condition
+                let condition = match jumps[condition_id] {
+                    Jumps::Condition(ref condition) => condition,
+                    _ => {
+                        panic!("condition not found")
                     }
-                    _=>{panic!("condition not found")}
                 };
 
                 let forward_id = condition.addr;
 
-                file_content.push_str(format!("    je forward_{}\n",forward_id).as_str());
-                file_content.push_str(format!("condition_{}:\n",condition_id).as_str());
-
+                file_content.push_str(format!("    je forward_{}\n", forward_id).as_str());
+                file_content.push_str(format!("condition_{}:\n", condition_id).as_str());
             }
-            ']'=>{
+            // ']' => {
+            TokenType::NonZeroJump => {
                 file_content.push_str("    mov rax, mem\n");
                 file_content.push_str("    add rax, QWORD[pointer]\n");
                 file_content.push_str("    mov al, byte[rax]\n");
                 file_content.push_str("    cmp al, 0\n");
 
                 let mut forward_id = 0;
-                for j in last_condition..len{
-                    if matches!(jumps[j], Jumps::Forward(_)){
+                for j in last_condition..len {
+                    if matches!(jumps[j], Jumps::Forward(_)) {
                         forward_id = j;
                         break;
                     }
                 }
 
-                last_condition = forward_id+1;
+                last_condition = forward_id + 1;
 
-                let forward = match jumps[forward_id]{
-                    Jumps::Forward(ref forward) => {
-                        forward
+                let forward = match jumps[forward_id] {
+                    Jumps::Forward(ref forward) => forward,
+                    _ => {
+                        panic!("forward not found")
                     }
-                    _=>{panic!("forward not found")}
                 };
 
                 let condition_id = forward.back_addr;
 
-                file_content.push_str(format!("    jne condition_{}\n",condition_id).as_str());
-                file_content.push_str(format!("forward_{}:\n",forward_id).as_str());
-
-
+                file_content.push_str(format!("    jne condition_{}\n", condition_id).as_str());
+                file_content.push_str(format!("forward_{}:\n", forward_id).as_str());
             }
-            _=>{}
+            _ => {}
         }
-
     }
 
     file_content.push_str("    mov rax, 60\n");
@@ -261,18 +431,17 @@ fn main() {
     file_content.push_str("    pointer: resb 8\n");
     file_content.push_str(format!("    mem: resb {} \n", mem_size).as_str());
 
-
     // create a new file
     let path = format!("{}", filename.replace(".bf", ".asm"));
     let path = Path::new(&path);
     let display = path.display();
 
-    let mut file = match File::create(&path){
+    let mut file = match File::create(&path) {
         Err(why) => panic!("Couldn't create {}: {}", display, why),
         Ok(file) => file,
     };
 
-    match file.write_all(file_content.as_bytes()){
+    match file.write_all(file_content.as_bytes()) {
         Err(why) => panic!("Couldn't write to {}: {}", display, why),
         Ok(_) => println!("Successfully wrote to {}", display),
     }
